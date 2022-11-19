2c76a058ac9ab30d6b91030b91966d45596ea1a7
jonasbender99 <jonas.bender@cognizant.com>
2022-11-19 19:47:47 +0100

    worked on search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,11 @@ import { LoginComponent } from './login/login.component';
 import { TopArtistsComponent } from './top-artists/top-artists.component';
 import { SearchInputComponent } from './search-input/search-input.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,8 +25,15 @@ import { SearchPageComponent } from './search-page/search-page.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
