e3ec143eef1eefd0ef14d076b5ab77b7e6fbb2a0
jonasbender <contact@jonasbender.net>
2022-12-03 23:40:18 +0100

    added transfer of artist to concert component

--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SearchService {
-  apiKey = "746bc9dc80924d48804b351fcba66356-87hbd7lFjPCkjQd91jtkgbNzR_6FE2lrfelE6r7zglgC7poxpj99h2WfwS_IfVP1tVyuueKJpi5JWz_4LP3Da059SDO1EQC-SUKHJdeQKDVjUmruRGZdlyC9tNSoI9Rk68i-Czw";
+  
 
   constructor(private http: HttpClient) { }
 
