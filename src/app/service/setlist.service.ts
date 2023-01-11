3a4ef38b7e268768205373915fc7046fb8f0be09
jonasbender <contact@jonasbender.net>
2023-01-11 23:14:20 +0100

    created setlist service

---
+++ src/app/service/setlist.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SetlistService {
+
+  constructor(
+    private http : HttpClient,
+
+  ) { }
+
+  getSetlist(setlistId: string){
+    return this.http.get('http://localhost:8080/api/tracks/setlistId')
+  }
+}
--- src/app/setlist/setlist.component.ts
+++ src/app/setlist/setlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-setlist',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetlistComponent implements OnInit {
 
-  constructor() { }
+  id!: string;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params); 
+        this.id = params['id'];
+      }
+    );
+
+
   }
 
 }
