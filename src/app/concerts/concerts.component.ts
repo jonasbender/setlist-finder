61a19b8e455260a73b5ea90abd56fdb6fc347f37
jonasbender <contact@jonasbender.net>
2023-04-25 00:27:22 +0200

    small changes

--- src/app/concerts/concerts.component.ts
+++ src/app/concerts/concerts.component.ts
@@ -5,76 +5,77 @@ import { SearchPageComponent } from '../search-page/search-page.component';
 import { ConcertService } from '../service/concert.service';
 
 @Component({
-  selector: 'app-concerts',
-  templateUrl: './concerts.component.html',
-  styleUrls: ['./concerts.component.css']
+	selector: 'app-concerts',
+	templateUrl: './concerts.component.html',
+	styleUrls: ['./concerts.component.css'],
 })
 export class ConcertsComponent implements OnInit {
-  
-  selectedArtist: any = "";
-  selectedConcertId!: string;
-  concerts: any;
+	selectedArtist: any = '';
+	selectedConcertId!: string;
+	concerts: any;
 
-  date: any;
-  month: any;
-  day: any;
-  year: any;
+	date: any;
+	month: any;
+	day: any;
+	year: any;
 
+	numberOfSongs: any;
 
-  numberOfSongs: any;
+	constructor(
+		private searchPageComponent: SearchPageComponent,
+		private concertService: ConcertService,
+		private router: Router,
+		private route: ActivatedRoute
+	) {}
 
-  constructor(
-    private searchPageComponent: SearchPageComponent,
-    private concertService: ConcertService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+	ngOnInit(): void {
+		this.selectedArtist = this.searchPageComponent.onSelected();
+		this.selectedArtist = this.selectedArtist.name;
+		console.log('selected Artist: ' + this.selectedArtist);
+		this.router.navigate([], {
+			relativeTo: this.route,
+			queryParams: { artist: this.selectedArtist },
+			skipLocationChange: false,
+		});
 
-  ngOnInit(): void {
-    this.selectedArtist = this.searchPageComponent.onSelected();
-    this.selectedArtist = this.selectedArtist.name;
-    console.log("test"+this.selectedArtist);
-    this.router.navigate([], {relativeTo: this.route, queryParams: {artist: this.selectedArtist}, skipLocationChange: false} );
+		this.concertService
+			.getConcertResults(this.selectedArtist)
+			.subscribe((data) => {
+				this.concerts = data;
+				console.log(this.concerts);
+			});
+	}
 
-    this.concertService.getConcertResults(this.selectedArtist).subscribe(
-      data => {
-        this.concerts = data;
-        console.log(this.concerts);
-      }
-    )
-  }
+	selectConcert(id: any) {
+		this.selectedConcertId = id;
+		this.reload();
+		this.router.navigate(['setlist'], {
+			queryParams: { id: id },
+			queryParamsHandling: 'merge',
+			skipLocationChange: false,
+		});
 
-  selectConcert(id: any) {
-    this.selectedConcertId = id; 
-    this.reload();
-    this.router.navigate(["setlist"], {queryParams: {id: id},
-    queryParamsHandling: 'merge',
-     skipLocationChange: false} );
-    
-    console.log(id);
-  }
+		console.log(id);
+	}
 
-  reload() {
-    this.router.navigate([], {relativeTo:this.route})
-  }
+	reload() {
+		this.router.navigate([], { relativeTo: this.route });
+	}
 
-  splitDate(dateString : string) {
-    var month = dateString.split("-")[1];
-    var day = dateString.split("-")[0];
-    var year = dateString.split("-")[2];
-    const date = new Date(+year, +month -1, +day);
-    return date;
-  }
+	splitDate(dateString: string) {
+		var month = dateString.split('-')[1];
+		var day = dateString.split('-')[0];
+		var year = dateString.split('-')[2];
+		const date = new Date(+year, +month - 1, +day);
+		return date;
+	}
 
-  calculateNumberOfSongs(sets : any) {
-    var numberOfSongs = 0;
-    for (let i = 0; i<sets.set.length; i++) {
-      var songArray = sets.set[i].song;
-      numberOfSongs += songArray.length;
-      
-    }
-    return numberOfSongs;
-  }
-
-
+	calculateNumberOfSongs(sets: any) {
+		var numberOfSongs = 0;
+		for (let i = 0; i < sets.set.length; i++) {
+			var songArray = sets.set[i].song;
+			numberOfSongs += songArray.length;
+		}
+		return numberOfSongs;
+	}
 }
--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,99 +1,110 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, finalize, fromEvent, Observable, Subject, Subscription, switchMap, tap } from 'rxjs';
+import {
+	debounceTime,
+	distinctUntilChanged,
+	filter,
+	finalize,
+	fromEvent,
+	Observable,
+	Subject,
+	Subscription,
+	switchMap,
+	tap,
+} from 'rxjs';
 import { MatAutocompleteModule } from '@angular/material';
-import {MatCommonModule} from '@angular/material/core';
+import { MatCommonModule } from '@angular/material/core';
 import { SearchService } from '../service/search.service';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+	selector: 'app-search-page',
+	templateUrl: './search-page.component.html',
+	styleUrls: ['./search-page.component.css'],
 })
-export class SearchPageComponent  {
-  
-  /*
+export class SearchPageComponent {
+	/*
   public artist! : string;
   public searchResults : any
   public subscription!: Subscription;
   */
 
-  searchArtistsCtrl = new FormControl();
-  filteredArtists: any;
-  isLoading = false;
-  errorMsg!: string;
-  minLengthTerm = 3;
-  selectedArtist: any = "";
+	searchArtistsCtrl = new FormControl();
+	filteredArtists: any;
+	isLoading = false;
+	errorMsg!: string;
+	minLengthTerm = 3;
+	selectedArtist: any = '';
 
-  @ViewChild('input', { static: true })
-  input!: ElementRef;
-  static selectedArtist: any;
+	@ViewChild('input', { static: true })
+	input!: ElementRef;
+	static selectedArtist: any;
+	showDiv: boolean = false;
 
-  constructor(
-    private searchService : SearchService,
-    private http : HttpClient,
-    private router : Router
-  )
-  {}
+	constructor(
+		private searchService: SearchService,
+		private http: HttpClient,
+		private router: Router,
+		private route: ActivatedRoute
+	) {}
 
-  onSelected() {
-    this.selectedArtist = this.selectedArtist;
-    console.log("selectedArtist: "+this.selectedArtist.name);
-    return this.selectedArtist;
-    
-  }
+	onSelected() {
+		this.selectedArtist = this.selectedArtist;
+		console.log('selectedArtist: ' + this.selectedArtist.name);
+		return this.selectedArtist;
+	}
 
-  displayWith(value: any) {
-    return value?.name
-  }
+	displayWith(value: any) {
+		return value?.name;
+	}
 
-  clearSelection() {
-    this.selectedArtist = "";
-    this.filteredArtists = [];
-  }
+	clearSelection() {
+		this.selectedArtist = '';
+		this.filteredArtists = [];
+	}
 
-  ngOnInit() {
-    this.searchArtistsCtrl.valueChanges
-      .pipe(
-        filter(res => {
-          return res !== null && res.length >= this.minLengthTerm
-        }),
-        distinctUntilChanged(),
-        debounceTime(300),
-        tap(() => {
-          this.errorMsg = "";
-          this.filteredArtists = [];
-          this.isLoading = true;
-        }),
-        switchMap(value => this.http.get("http://localhost:8080/api/search/"+ value)
-          .pipe(
-            finalize(() => {
-              this.isLoading = false;
-            })
-          )
-        )
-      )
-      .subscribe((data: any) => {
-        if (data == undefined) {
-          this.errorMsg = data['Error'];
-          this.filteredArtists = [];
-          console.log("error in Datasearch");
-        } else {
-          this.errorMsg = "";
-          this.filteredArtists = data ;
-        }
-        console.log("filtered Artists" + this.filteredArtists);
-      });
-  }
+	ngOnInit() {
+		this.searchArtistsCtrl.valueChanges
+			.pipe(
+				filter((res) => {
+					return res !== null && res.length >= this.minLengthTerm;
+				}),
+				distinctUntilChanged(),
+				debounceTime(300),
+				tap(() => {
+					this.errorMsg = '';
+					this.filteredArtists = [];
+					this.isLoading = true;
+				}),
+				switchMap((value) =>
+					this.http
+						.get('http://localhost:8080/api/search/' + value)
+						.pipe(
+							finalize(() => {
+								this.isLoading = false;
+							})
+						)
+				)
+			)
+			.subscribe((data: any) => {
+				if (data == undefined) {
+					this.errorMsg = data['Error'];
+					this.filteredArtists = [];
+					console.log('error in Datasearch');
+				} else {
+					this.errorMsg = '';
+					this.filteredArtists = data;
+				}
+				console.log('filtered Artists' + this.filteredArtists);
+			});
+	}
 
-  get SelectedArtist() {
-    return this.selectedArtist;
-  }
-  
-  
-  /*
+	get SelectedArtist() {
+		return this.selectedArtist;
+	}
+
+	/*
   Submit() {
     
     this.subscription = this.searchService.getResults(this.artist).subscribe(
@@ -108,7 +119,4 @@ export class SearchPageComponent  {
     this.subscription.unsubscribe()
   }
   */
-
-  
-
 }
