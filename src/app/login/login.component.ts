35f5270d8dffa8b30763a25aabafcc7091220564
jonasbender <contact@jonasbender.net>
2023-05-05 01:09:16 +0200

    refactoring of component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,31 +4,26 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+	public response: any;
 
-  public response : any;
+	constructor(
+		private router: Router,
+		private authService: AuthenticationService
+	) {}
 
-  constructor(
-    private router : Router,
-    private authService : AuthenticationService,
-  ) { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  Submit() {
-  this.authService.getSpotifyUserLogin().subscribe(
-    data => {
-      this.response = data;
-      console.log(this.response);
-      window.location.href=this.response
-    }
-  )
-  }
-
-
+	Submit() {
+		const currentUrl = window.location.href;
+		this.authService.getSpotifyUserLogin(currentUrl).subscribe((data) => {
+			this.response = data;
+			console.log(this.response);
+			window.location.href = this.response;
+		});
+	}
 }
